fd9ed026c571191990f9e37e672e8e22ab51072b
Pavel Kamyshev <Pavel_Kamyshev@epam.com>
2017-09-22 15:52:34 +0300

    modify files

--- agent.sh
+++ agent.sh
@@ -1,11 +1,11 @@
-echo "Adding repos..."
+echo "Adding repos and install"
 wget http://repo.zabbix.com/zabbix/3.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_3.2-1+trusty_all.deb
 dpkg -i 'zabbix-release_3.2-1+trusty_all.deb'
 apt-get -y update
 export DEBIAN_FRONTEND=noninteractive
 echo "Done."
 
-echo "Installing zabbix-agent..."
+echo "Installing zabbix-agent"
 apt-get install -y zabbix-agent &>/dev/null
 sed -i "s/Server=127.0.0.1/Server=kamyshevzabbix/" /etc/zabbix/zabbix_agentd.conf
 echo "Done."
